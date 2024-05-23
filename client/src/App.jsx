fea94c086822d2a6bf99051f76f369dff657f1a2
mohammadnoor <mohammadnoorcse@gmail.com>
2024-05-23 17:16:43 +0600

    working at project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,73 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { io } from "socket.io-client";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 const App = () => {
-  const socket = io("http://localhost:3000");
+  // const socket = io("http://localhost:3000");
+  const socket = useMemo(()=>io("http://localhost:3000"),[]);
+  const[message,setMessage] = useState("");
+  const [room,setRoom] = useState("");
+  const[messages,setMessages] = useState([]);
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    // socket.emit("message",message);
+    socket.emit("object-message",{message,room})
+    setMessage("");
+    setRoom("");
+  };
   
   useEffect(() =>{
     socket.on("connect",()=>{
       console.log("connected",socket.id);
-    })
+    });
+    socket.on("welcome",(s)=>{
+      console.log(s)
+    });
+
+    socket.on("receive-message",(data) => {
+      // console.log(data)
+      setMessages((messages)=>[...messages,data])
+    });
+
+    return () =>{
+      socket.disconnect();
+    }
   },[])
   return (
-    <div>App</div>
+    <Container maxWidth="sm">
+      <Box sx={{height:200}}/>
+      <Typography variant='h3' component="div" gutterBottom>
+        Welcome to Socket.io
+      </Typography>
+       <form onSubmit={handleSubmit}>
+
+         <TextField value={message} 
+         onChange={(e)=>setMessage(e.target.value)}
+         id="outlined-basic" label="Message" variant='outlined'/>
+
+         <TextField value={room} 
+         onChange={(e)=>setRoom(e.target.value)}
+         id="outlined-basic" label="Room" variant='outlined'/>
+
+         <Button type='submit' variant='contained' color='primary'>Send</Button>
+       </form>
+
+       <Stack>
+        {messages.map((m, i) => (
+          <Typography key={i} variant="h6" component="div" gutterBottom>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
+
+    </Container>
   )
 }
 
--- server/app.js
+++ server/app.js
@@ -25,8 +25,33 @@ app.use(
 
 io.on("connection",(socket)=>{
    console.log("User Connected");
-   console.log("Id",socket.id);
+//    console.log("Id",socket.id);
+   socket.emit("welcome",`Welcome to the server,${socket.id}`);
+
+//    socket.on("message",(data)=>{
+//     //  console.log(data)
+//     socket.broadcast.emit("receive-message",data); // emit->send data
+//    });
+
+//    socket.on("object-message",(data)=>{
+//     //  console.log(data)
+//     socket.broadcast.emit("receive-message",data); // emit->send data
+//    });
+
+   socket.on("object-message",({room,message})=>{
+    //  console.log(data)
+    socket.to(room).emit("receive-message",message); 
+   });
+
+   socket.on("disconnect",()=>{
+    console.log("User Disconnected.",socket.id)
+   })
 })
 server.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
+
+//  emit-> send data
+// on->receive data
+// broadcast->nije send kora data nijer kache show korbe na
+// to(Id)->one to one send message . apni jake sms tar id to  er bitore send korte hobe
